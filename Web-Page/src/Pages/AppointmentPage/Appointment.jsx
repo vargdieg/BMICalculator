b3c9ed96a5141f6cd3b634b0564f143209e822e9
dfvargas <dfvargas@bancolombia.com.co>
2024-10-12 14:05:20 -0500

    Se deja un comentario para probar flujo de automatizacion

--- Web-Page/src/Pages/AppointmentPage/Appointment.jsx
+++ Web-Page/src/Pages/AppointmentPage/Appointment.jsx
@@ -14,6 +14,8 @@ import ModalOption from '../../Components/ModalAcceptCancel/ModalOption';
 
 export default function Appointment(){
 
+    //Se deja esta linea como comentarios
+
     const Session = getCurrentSession();
     if(Session === null){
         window.location.href = "/";
