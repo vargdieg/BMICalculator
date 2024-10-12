640681c3eff90f5fdf3b67a26dfd29bb85d19db1
dfvargas <dfvargas@bancolombia.com.co>
2024-10-12 12:40:18 -0500

    Se adiciona un comentario para validar despliegue automatico

--- Web-Page/src/Pages/AppointmentPage/Appointment.jsx
+++ Web-Page/src/Pages/AppointmentPage/Appointment.jsx
@@ -14,6 +14,8 @@ import ModalOption from '../../Components/ModalAcceptCancel/ModalOption';
 
 export default function Appointment(){
 
+    //Se crea un comentario para adicionar
+
     const Session = getCurrentSession();
     if(Session === null){
         window.location.href = "/";
