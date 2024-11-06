1a0aa49b80fbf16c26a078ed2dd0b8cd5b88e193
dfvargas <dfvargas@bancolombia.com.co>
2024-11-06 17:19:30 -0500

    Se actualiza el template html para no mostrar bullet points

--- Email-Templates/script/create-templates.mjs
+++ Email-Templates/script/create-templates.mjs
@@ -1,4 +1,4 @@
-import { CreateTemplateCommand,GetTemplateCommand,SESClient,TemplateDoesNotExistException } from "@aws-sdk/client-ses";
+import { CreateTemplateCommand,GetTemplateCommand,UpdateTemplateCommand,SESClient,TemplateDoesNotExistException } from "@aws-sdk/client-ses";
 import fs from 'fs';
 import path from 'path';
 
@@ -11,21 +11,25 @@ try{
     const lines = data.split(/[\n\r]+/g);
     lines.forEach(async (line)=>{
         const [TemplateName,emailSubject,filePathHtml,filePathText] = line.split(/[\s]+/g);
-        if(templateExists(TemplateName)){
-            console.log(`El template con el nombre ${TemplateName} ya existe`)
-        }else{
-            const textPath = path.resolve(__dirname, `../${filePathText}`);
-            const htmlPath = path.resolve(__dirname, `../${filePathHtml}`);
-            const TextPart = fs.readFileSync(textPath,'utf8');
-            const HtmlPart = fs.readFileSync(htmlPath,'utf8');
-            const templateJson = {
-                Template:{
-                    TemplateName,
-                    SubjectPart: emailSubject.replaceAll('_',' '),
-                    TextPart,
-                    HtmlPart
-                }
+        const textPath = path.resolve(__dirname, `../${filePathText}`);
+        const htmlPath = path.resolve(__dirname, `../${filePathHtml}`);
+        const TextPart = fs.readFileSync(textPath,'utf8');
+        const HtmlPart = fs.readFileSync(htmlPath,'utf8');
+        const templateJson = {
+            Template:{
+                TemplateName,
+                SubjectPart: emailSubject.replaceAll('_',' '),
+                TextPart,
+                HtmlPart
             }
+        }
+        if(templateExists(TemplateName)){
+            console.log(`El template con el nombre ${TemplateName} ya existe, se procede a actualizar`);
+            const command = new UpdateTemplateCommand(templateJson);
+            const response = await client.send(command);
+            console.log(response);
+        }else{
+            console.log(`El template no existe, se procede a crearlo`);
             const command = new CreateTemplateCommand(templateJson);
             const response = await client.send(command);
             console.log(response);
